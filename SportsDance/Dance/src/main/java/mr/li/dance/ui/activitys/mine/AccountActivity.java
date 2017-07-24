165a84b63875022d379b04aba27909f7d5e1c7af
追风中Zzz <380877655@qq.com>
2017-07-24 18:01:34 +0800

    7.24

---
+++ SportsDance/Dance/src/main/java/mr/li/dance/ui/activitys/mine/AccountActivity.java
@@ -0,0 +1,61 @@
+package mr.li.dance.ui.activitys.mine;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import mr.li.dance.R;
+import mr.li.dance.ui.activitys.base.BaseListActivity;
+
+
+/**
+ * 作者: yjd
+ * 版本: 1.0
+ * 创建日期: 2017/7/24
+ * 描述: 首页-我的-我的账户
+ * 修订历史:
+ */
+
+public class AccountActivity extends BaseListActivity {
+
+
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_account;
+    }
+
+    @Override
+    public void initViews() {
+        setTitle("账单明细");
+        mHeadRightText.setVisibility(View.VISIBLE);
+        mHeadRightText.setBackgroundResource(R.drawable.mine_withdrawdeposit_ref);
+        mHeadRightText.setPadding(0,5,10,5);
+        mHeadRightText.setText("提现");
+    }
+
+    /**
+     * 点击右边按钮
+     * @param v
+     */
+    @Override
+    public void onHeadRightButtonClick(View v) {
+        super.onHeadRightButtonClick(v);
+        startActivity(new Intent(this,WithdrawdepositActivity.class));
+    }
+
+    public static void lunch(Context context) {
+        context.startActivity(new Intent(context, AccountActivity.class));
+    }
+
+
+    @Override
+    public void itemClick(int position, Object value) {
+
+    }
+    @Override
+    public RecyclerView.Adapter getAdapter() {
+        return null;
+    }
+
+}
---
+++ SportsDance/Dance/src/main/java/mr/li/dance/ui/activitys/mine/WithdrawdepositActivity.java
@@ -0,0 +1,23 @@
+package mr.li.dance.ui.activitys.mine;
+
+import mr.li.dance.ui.activitys.base.BaseActivity;
+
+/**
+ *  描述: 提现页面
+ * Author     : yjd
+ * Date       : 2017/7/24
+ */
+
+public class WithdrawdepositActivity extends BaseActivity{
+
+    @Override
+    public int getContentViewId() {
+        return 0;
+    }
+
+    @Override
+    public void initViews() {
+       setTitle("提现");
+    }
+
+}
